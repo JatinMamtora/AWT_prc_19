cc347cb1eb68320d4f8ba2323810010288fce5b9
JatinMamtora <115716610+JatinMamtora@users.noreply.github.com>
2023-10-10 21:23:21 +0530

    index.js

--- index.js
+++ index.js
@@ -1,51 +1,60 @@
-const express = require(&quot;express&quot;);
-const nodemailer = require(&quot;nodemailer&quot;);
-const bodyParser = require(&quot;body-parser&quot;);
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 const transporter = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-    user: 'jatinumamtoragmail.com',
-    pass: 'hello',
-  },
+  service: "Gmail",
+  auth: {
+    user: "jatinumamtora@gmail.com",
+    pass: "Jatin@12",
+  },
 });
 
-app.get("/", (req, res){
-  res.send(`
-<h1>Email Subscription</h1>
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>Email Subscription</h1>
     <form method="POST" action="/subscribe">
       <label>Email:</label>
       <input type="email" name="email" required>
       <button type="submit">Subscribe</button>
     </form>
-  `);
+  `);
 });
-// Handle POST request to subscribe
-app.post(&quot;/subscribe&quot;, (req, res) {
-  const email = req.body.email;
-  const mailOptions = {
-    from: "YourEmailAddress",
-    to: email,
-    subject: "Welcome to Our Newsletter",
-    text: "Thank you",
-  };
 
-  transporter.sendMail(mailOptions, (error, info) {
-    if (error) {
-      console.error(error);
-      res.status(500).send("Subscription failed");
-    } else {
-      console.log("Email sent: + info.response);
-      res
-        .status(200)
-        .send(;Subscription successful! Check your email for confirmation");
-    }
-  });
+
+app.post("/subscribe", (req, res) => {
+  const email = req.body.email;
+
+  const mailOptions = {
+    from: "YourEmailAddress",
+    to: email,
+    subject: "Welcome to Our Newsletter",
+    text: "Thank you for subscribing to our newsletter!",
+  };
+
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send("Subscription failed. Please try again later.");
+    } else {
+      console.log("Email sent: " + info.response);
+      res
+        .status(200)
+        .send("Subscription successful! Check your email for confirmation.");
+    }
+  });
 });
-app.listen(PORT, ()  {
-  console.log(`Server is running on port ${PORT}`);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
